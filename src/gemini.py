ba5938013ff512d49912299d1fe105c61cf5a155
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2024-04-25 10:36:36 +0530

    chore: handled the quota limit exceeded exception

--- src/gemini.py
+++ src/gemini.py
@@ -1,5 +1,5 @@
 import google.generativeai as genai
-
+from exception import handle_resource_exhausted
 class Gemini:
     def __init__(self, api_key):
         genai.configure(api_key=api_key)
@@ -28,13 +28,16 @@ class Gemini:
         for fileName, displayName in zip(self.files_uploaded, self.upload_files_display_names):
             print(f"Display Name: {displayName} => File Name: {fileName}")
 
+    @handle_resource_exhausted
     def run(self, prompt, file):
         response = self.model.generate_content([prompt, file])
-        print(f"Generated content: {response.text}")
+        return response.text
+
+    @handle_resource_exhausted
+    def generate_content(self, prompt):
+        return self.model.generate_content([prompt]).text
 
     def infer(self, prompt, file_name):
         file = self.get_file(file_name)
         self.run(prompt, file)
 
-    def generate_content(self, prompt):
-        return self.model.generate_content([prompt]).text
